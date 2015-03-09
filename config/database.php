00e7800abccbaf4636661aa072a34918196efcff
burgosa <alvaro.burgos@outlook.com>
2015-03-09 13:46:30 -0600

    database 2

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST'),
-			'database'  => env('DB_DATABASE'),
-			'username'  => env('DB_USERNAME'),
-			'password'  => env('DB_PASSWORD'),
+			'host'      => $_ENV['DB_HOST'],
+			'database'  => $_ENV['DB_DATABASE'],
+			'username'  => $_ENV['DB_USERNAME'],
+			'password'  => $_ENV['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
